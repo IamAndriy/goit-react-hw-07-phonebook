1d58ac3b6a6477695735339620d3f83977093c9b
Olga Dzubak <>
2025-02-15 23:40:50 +0200

    Update contactsSlice.js

--- src/redux/contactsSlice.js
+++ src/redux/contactsSlice.js
@@ -32,18 +32,17 @@ function handleRejected (state, action){
 function handleFulfilledGet (state, action){ 
     state.isLoading = false; 
     state.error = null;
-    state.items = action.payload
+    state.items = [...action.payload];
 }
 function handleFulfilledAdd (state, action){ 
     state.isLoading = false; 
     state.error = null;
-    state.items.push(action.payload);
+    state.items= [...state.items, action.payload];
 }
 function handleFulfilledDelete (state, action){ 
     state.isLoading = false; 
     state.error = null;
-    const index = state.items.findIndex(contact => contact.id === action.payload.id);
-    state.items.splice(index, 1)
+    state.items = state.items.filter(item => item.id !== action.payload.id);
 }
 //----------------------------------------------------------------
 
